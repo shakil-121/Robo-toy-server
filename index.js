80a50ba098f05607fb02ddb531dd6d7b6a3f7756
shakil-121 <shahadatshakil76@gmail.com>
2023-05-21 07:50:21 -0700

    Delete Done

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-//pass:
-//user:
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bfdmw9d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
